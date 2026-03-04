891dc3b3eb6ac4a367263ac8f4789697ea6f39ce
AxelleDrd <axelle.drouard@orange.fr>
2026-03-04 17:50:17 +0100

    Fix Jeu et Consoles dans Test

--- Nintendo-5/src/test/java/nintendo/test/Test.java
+++ Nintendo-5/src/test/java/nintendo/test/Test.java
@@ -3,26 +3,29 @@ package nintendo.test;
 import nintendo.model.Boutique;
 import nintendo.model.Client;
 import nintendo.model.Console;
+import nintendo.model.Hybride;
 import nintendo.model.Jeu;
+import nintendo.model.Portable;
+import nintendo.model.Salon;
 
 public class Test {
 
 	public static void main(String[] args) {
 
-		Console c1 = new Console("PS4");
-		Console c2 = new Console("PS5");
-		Console c3 = new Console("Xbox One");
+		Console c1 = new Salon("PS4");
+		Console c2 = new Hybride("Switch");
+		Console c3 = new Portable("Gameboy");
 		
-		Jeu jeu1 = new Jeu("Mario kart",c1);
-		Jeu jeu2 = new Jeu("Stardew Valley",c2);
-		Jeu jeu3 = new Jeu("Assassin's Creed",c2);
-		Jeu jeu4 = new Jeu("Batman",c3);
-		Jeu jeu5 = new Jeu("Sonic",c3);
+		Boutique boutique1 = new Boutique("Minecraft", "128 rue du dauphiné");
+		
+		Jeu jeu1 = new Jeu("Mario kart",c1, boutique1);
+		Jeu jeu2 = new Jeu("Stardew Valley",c2, boutique1);
+		Jeu jeu3 = new Jeu("Assassin's Creed",c2, boutique1);
+		Jeu jeu4 = new Jeu("Batman",c3, boutique1);
+		Jeu jeu5 = new Jeu("Sonic",c3, boutique1);
 		
 		Client client1 = new Client("Doe", "John");
 		Client client2 = new Client("Doe", "Jane");
-		
-		Boutique boutique1 = new Boutique("Minecraft", "128 rue du dauphiné");
 	
 	}
 
