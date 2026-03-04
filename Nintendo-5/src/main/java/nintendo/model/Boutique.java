9756b0d4c5f5075337af0ec04f7c9ca9811024aa
jhomrlou-prog <jhomrlou@gmail.com>
2026-03-04 16:45:31 +0100

    classe boutique

---
+++ Nintendo-5/src/main/java/nintendo/model/Boutique.java
@@ -0,0 +1,25 @@
+package nintendo.model;
+
+public class Boutique {
+	private String nom;
+	private String adresse;
+	public Boutique(String nom, String adresse) {
+		this.nom = nom;
+		this.adresse = adresse;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getAdresse() {
+		return adresse;
+	}
+	public void setAdresse(String adresse) {
+		this.adresse = adresse;
+	}
+	
+	
+
+}
